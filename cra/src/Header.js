918b8051dc81db9a8bde0250e49c236a1ca151bc
brandontjl <brandonteo1996@gmail.com>
2023-05-25 01:27:33 +0800

    Setting up Home Page

---
+++ cra/src/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <div className="header" >
+            <img src="https://i.imgur.com/K9AfZE5.png" alt="premlogo"></img>
+            <div className="topnav">
+                <a class="active" href="#home"></a>
+            </div>
+        </div >
+    )
+
+}
+
+export default Header
